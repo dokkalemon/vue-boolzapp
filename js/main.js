b536ea5006167bc587516faffb55575226cb2b27
dokkalemon <dottori.roberto@gmail..com>
2021-11-17 14:38:14 +0100

    add function selectChat

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+/* 
+//TODO: Milestone 1
+Replica della grafica con la possibilità di avere messaggi 
+scritti dall’utente (verdi) e dall’interlocutore (bianco) 
+assegnando due classi CSS diverse.
+Le icone non hanno funzionalità ma sono solo 
+presentazionali.
+
+Visualizzazione dinamica della lista contatti: tramite la 
+direttiva v-for, visualizzare nome e immagine di ogni
+ contatto
+
+
+//TODO: Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, 
+visualizzare tutti i messaggi relativi al contatto attivo all’interno
+ del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+
+
+//TODO: Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte 
+bassa e digitando “enter” il testo viene aggiunto al thread sopra,
+ come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, 
+l’utente riceverà un “ok” come risposta, che apparirà dopo 
+1 secondo.
+*/
+
+
+
+
+
+
+
+
+
 /* Vue */
 const app = new Vue({
     el: '#app',
@@ -85,7 +122,14 @@ const app = new Vue({
                     }
                 ],
             },
-        ]
+        ],
+
+        activeChat: 0,
         
+    },
+    methods: {
+        selectChat(index) {
+            this.activeChat = index;   
+        }
     }
 }) 
